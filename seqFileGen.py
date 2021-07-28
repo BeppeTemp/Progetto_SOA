7fe23e7ba9f7256ea2d23dc37a2fd11fb8ebbf0c
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-07-28 10:34:41 +0200

    seq file

--- seqFileGen.py
+++ seqFileGen.py
@@ -1,8 +1,8 @@
 import base64, json, os
 
-img_path = "mnt/c/Users/giuse/Desktop/IMAGE/IMAGE/"
-json_path = "mnt/c/Users/giuse/Desktop/JSON/JSON/"
-result_path = "mnt/c/Users/giuse/Desktop/RESULT/"
+img_path = "IMAGE/"
+json_path = "JSON/"
+result_path = "RESULT/"
 
 def convertoToBase64(fname):
     with open(fname, "rb") as img_file:
@@ -20,29 +20,22 @@ def createSingleJSON(fname, img_list):
     if fname+".jpg" in img_list:
         j["image"] = str(convertoToBase64(img_path + fname+".jpg"))[1:].replace("'","")
 
-        '''result = open("result.jpg", "wb")
-        result.write(convertToImage(j["image"]))
-        result.close()'''
-
     return j
 
 def createJSON(img_list, json_list):
-    with open(result_path + "DataSet.json", "w") as data_set:
-        data_set.write("[")
+    with open(result_path + "DataSet_Grezzo_.json", "w") as data_set:
         i = 0
 
         for f in json_list:
             i = i + 1
-            print("Processate " + str(i) + "immagini")
+            if (i % 10000 == 0):
+                print("Processate " + str(i) + " immagini.")
 
             f = f.replace('_keypoints.json','')
             j = createSingleJSON(f, img_list)
             j["id"] = f
             data_set.write(json.dumps(j))
-            if i != len(json_list):
-                data_set.write(",")
 
-        data_set.write("]")
         data_set.close()
 
 
@@ -54,8 +47,3 @@ print("Ordinamento completato")
 createJSON(img_list, json_list)
 
 print("Qualcosa è esploso ?")
-
-
-'''#Salvo il JSON finale
-with open("RESULT/" + fname + ".json", "w") as json_result:
-json_result.write(json.dumps(j))'''
