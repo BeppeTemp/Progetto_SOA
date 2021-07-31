208313d33978d8b15f6cc8b949d80a31e0aa7ec8
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-07-31 12:59:23 +0200

    test

--- src/main/java/SOA_Project.java
+++ src/main/java/SOA_Project.java
@@ -1,27 +1,30 @@
 import java.util.List;
 
-import javax.xml.crypto.Data;
-
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
+
 import org.apache.spark.sql.SparkSession;
 
 import org.apache.spark.api.java.function.ForeachFunction;
 
 public class SOA_Project {
+  
+  public static void dataSetProcessing(DataSetRow row){
+
+  } 
+
   public static void main(String[] args) {
     SparkSession spark = SparkSession.builder().appName("SOA Project").getOrCreate();
 
-    // Java Bean (data class) used to apply schema to JSON data
+    //Creazione java bean per la definizione dello schema del DataSet
 		Encoder<DataSetRow> dataSetEncoder = Encoders.bean(DataSetRow.class);
-
     String path = "Arienzo-Giordano-Scotti/DataSetPic.json";
-
+    //Lettura del DataSet
     Dataset<DataSetRow> dataset = spark.read().json(path).as(dataSetEncoder);
 
-    dataset.foreach((ForeachFunction<DataSetRow>) row -> System.out.println(row));
+    //Iterazione del DataSet
+    dataset.foreach((ForeachFunction<DataSetRow>) row -> System.out.println(row.getId()));
 
     spark.stop();
   }
