7f26655eb651af10b2afefec8a89ea403b4b5d48
prova <fjfjfj>
2021-07-31 10:50:39 +0200

    Java Version For exam Soa

---
+++ src/main/java/unisa/spark/exam/App.java
@@ -0,0 +1,13 @@
+package unisa.spark.exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/unisa/spark/exam/AppTest.java
@@ -0,0 +1,20 @@
+package unisa.spark.exam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
