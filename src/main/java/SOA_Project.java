a60159e86e0ac9a7873a3809406de93ca6d24e67
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-07-31 11:43:59 +0200

    Test 2

--- src/main/java/SOA_Project.java
+++ src/main/java/SOA_Project.java
@@ -1,12 +1,20 @@
-import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
 
 public class SOA_Project {
   public static void main(String[] args) {
-    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    SparkSession spark = SparkSession.builder().appName("SOA Project").getOrCreate();
 
+    // Java Bean (data class) used to apply schema to JSON data
+		Encoder<DataSetRow> dataSetEncoder = Encoders.bean(DataSetRow.class);
 
-    System.out.println("finito");
+    String path = "Arienzo-Giordano-Scotti/DataSetPic.json";
+
+    Dataset<DataSetRow> dataset = spark.read().json(path).as(dataSetEncoder);
+
+    dataset.show();
 
     spark.stop();
   }
